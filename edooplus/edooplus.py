37f163feeb0b73e860450faa72a90eac488e5be0
Jiri <63856816+cahaj@users.noreply.github.com>
2023-03-05 13:31:13 +0100

    Update edooplus.py

--- edooplus/edooplus.py
+++ edooplus/edooplus.py
@@ -3,39 +3,29 @@ from pcconfig import config
 
 import pynecone as pc
 
+from .base_state import State
+
+from .pages.index import index
+from .pages.settings import settings
+
 docs_url = "https://pynecone.io/docs/getting-started/introduction"
 filename = f"{config.app_name}/{config.app_name}.py"
 
 
-class State(pc.State):
-    """The app state."""
-
-    pass
-
-
-def index() -> pc.Component:
-    return pc.center(
-        pc.vstack(
-            pc.heading("Welcome to Pynecone!", font_size="2em"),
-            pc.box("Get started by editing ", pc.code(filename, font_size="1em")),
-            pc.link(
-                "Check out our docs!",
-                href=docs_url,
-                border="0.1em solid",
-                padding="0.5em",
-                border_radius="0.5em",
-                _hover={
-                    "color": "rgb(107,99,246)",
-                },
-            ),
-            spacing="1.5em",
-            font_size="2em",
-        ),
-        padding_top="10%",
-    )
-
 
 # Add state and page to the app.
 app = pc.App(state=State)
-app.add_page(index)
+
+app.add_page(index,
+    title="Edookit+",
+    description="Edookit wrapper written in python providing better experience and QOL features.",
+    image="/edookit.png",
+)
+
+app.add_page(settings, route="/settings",
+    title="Edookit+",
+    description="Edookit wrapper written in python providing better experience and QOL features.",
+    image="/edookit.png",
+)
+
 app.compile()
